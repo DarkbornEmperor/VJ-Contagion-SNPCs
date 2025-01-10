f5aa247fc79b33e017292d70dafd73423139fbd5
Darkborn <darkandrai1987@gmail.com>
2025-01-10 09:53:38 -0500

    Update init.lua

--- lua/entities/npc_vj_con_zcarrier/init.lua
+++ lua/entities/npc_vj_con_zcarrier/init.lua
@@ -8,16 +8,20 @@ include("shared.lua")
 -----------------------------------------------*/
 ENT.StartHealth = 520 // 220
 ENT.FlinchChance = 30
-/*ENT.IdleSoundPitch = VJ.SET(85, 85)
-ENT.CombatIdleSoundPitch = VJ.SET(85, 85)
-ENT.InvestigateSoundPitch = VJ.SET(85, 85)
-ENT.AlertSoundPitch = VJ.SET(85, 85)
-ENT.CallForHelpSoundPitch = VJ.SET(85, 85)
-ENT.BeforeMeleeAttackSoundPitch = VJ.SET(85, 85)
-ENT.PainSoundPitch = VJ.SET(85, 85)
-ENT.DeathSoundPitch = VJ.SET(85, 85)*/
+ENT.IdleSoundPitch = VJ.SET(75, 75)
+ENT.CombatIdleSoundPitch = VJ.SET(75, 75)
+ENT.InvestigateSoundPitch = VJ.SET(75, 75)
+ENT.AlertSoundPitch = VJ.SET(75, 75)
+ENT.CallForHelpSoundPitch = VJ.SET(75, 75)
+ENT.BeforeMeleeAttackSoundPitch = VJ.SET(75, 75)
+ENT.PainSoundPitch = VJ.SET(75, 75)
+ENT.DeathSoundPitch = VJ.SET(75, 75)
 -- Custom
 ENT.Zombie_Sprinter = true
+---------------------------------------------------------------------------------------------------------------------------------------------
+function ENT:ZombieVoices()
+    self:ZombieVoice_George()
+end
 /*-----------------------------------------------
     *** Copyright (c) 2012-2025 by DrVrej, All rights reserved. ***
     No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
