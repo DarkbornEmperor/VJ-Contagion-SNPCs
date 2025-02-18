5e63e446fc5e904af44d5db00a375f9a3460439b
Darkborn <darkandrai1987@gmail.com>
2025-02-18 04:21:26 -0500

    Update to latest VJ Base

--- lua/entities/npc_vj_con_zscreamer/shared.lua
+++ lua/entities/npc_vj_con_zscreamer/shared.lua
@@ -3,8 +3,6 @@ ENT.Type = "ai"
 ENT.PrintName = "Screamer Zombie"
 ENT.Author = "Darkborn"
 ENT.Contact = "http://steamcommunity.com/groups/vrejgaming"
-ENT.Purpose = "Spawn it and fight with it!"
-ENT.Instructions = "Click on the spawnicon to spawn it."
 ENT.Category = "Contagion"
 
 ENT.IsContagionZombie = true
